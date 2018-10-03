a835a07be20e0611dc43701b5f35bc4758e33dee
Brandon <mercadobrandon33@yahoo.com>
2018-10-02 19:52:52 -0700

    Create Skeleton Home Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div className="container">
+        <p className="text-center display-4">
+            Donna Choeun <br />
+             Photography 
+        </p>
+
+        <nav className="navbar navbar-expand-sm display-4 justify-content-center">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Galleries</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Investments</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Blog</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="carousel slide" data-ride="carousel">
+          <div className=" carousel-inner home_gallery">
+            <div className="carousel-item active">
+              <img src={require("./cat1.jpg")} />
+            </div>
+            <div className="carousel-item">
+              <img src={require("./cat2.jpg")} />
+            </div>
+          </div>
+
+          <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
           </a>
-        </header>
+          <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </a>
+        </div>
+
+        <div className="text-center">
+          <div className="display-4">
+            Follow me on Social Media:
+          </div>
+          <img src={require(".//F_Logo_Online_09_2018/Color/PNG/flogo_RGB_HEX-72.png")} />
+          <img style={{height: 72, width:72, marginLeft: 50}} src={require("./GlyphLogo_May2016_Onlinev2/glyph-logo_May2016.png")} />
+        </div>
+
       </div>
+
     );
   }
 }
