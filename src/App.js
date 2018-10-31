61af16125cf7b50dd38cdeea9c4521a240949ec3
Brandon <mercadobrandon33@yahoo.com>
2018-10-31 00:07:17 -0700

    fixed home page slider

--- src/App.js
+++ src/App.js
@@ -10,17 +10,30 @@ class App extends Component {
     super(props);
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
 
     this.state = {
-      homepics: []
+      homepics: [],
+      width: 0
   }
 }
 
 componentDidMount() {
+  this.updateWindowDimensions();
+  window.addEventListener('resize', this.updateWindowDimensions);
+
   axios.get('https://donna-photography-api.herokuapp.com/api/homepics')
       .then(res => {
           this.setState({homepics: res.data})
       })
+}
+
+componentWillUnmount() {
+  window.removeEventListener('resize', this.updateWindowDimensions);
+}
+
+updateWindowDimensions() {
+  this.setState({ width: window.innerWidth, height: window.innerHeight });
 }
 
   next() {
@@ -38,59 +51,28 @@ componentDidMount() {
       infinite: true,
       speed: 500,
       arrows: false,
-      slidesToShow: 3,
+      slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
       pauseOnHover: false,
-      centerMode: true,
-      variableHeight: true,
+      centerMode: false,
+      variableWidth: true,
       responsive: [
           {
-            breakpoint: 1024,
-            settings: {
-              dots: false,
-              infinite: true,
-              speed: 500,
-              arrows: false,
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              autoplay: true,
-              pauseOnHover: false,
-              centerMode: true,
-              variableHeight: true,
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              dots: false,
-              infinite: true,
-              speed: 500,
-              arrows: false,
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              autoplay: true,
-              pauseOnHover: false,
-              centerMode: true,
-              variableHeight: true,
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              dots: false,
-              infinite: true,
-              speed: 500,
-              arrows: false,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: true,
-              pauseOnHover: false,
-              centerMode: false,
-              variableHeight: true,
-            }
+              breakpoint: 480,
+              settings: {
+                  dots: false,
+                  infinite: true,
+                  speed: 500,
+                  arrows: false,
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  autoplay: true,
+                  pauseOnHover: false,
+                  variableWidth: false
+              },
           }
-        ]
+      ]
     }
 
     if(this.state.homepics.length == 0)
@@ -115,13 +97,40 @@ componentDidMount() {
         <div className="background-container">
            <Slider className="gallery-slider-container" {...settingsPictures} ref={slider => (this.slider = slider)}>
                       {this.state.homepics.map(homepic => {
-                          return (
-                            <div>
-                            <div>
-                                <div className="gallery-pic" style={{backgroundImage: `url("${homepic.image_url}")`}}></div>
-                            </div>
-                        </div>
-                          );
+                          if(this.props.match.params.gallery.toLowerCase() == homepic.gallery)
+                          {
+                              let width;
+  
+                              if(this.state.width < 500)
+                              {
+                                  return (
+                                      <div>
+                                          <div>
+                                          <div className="gallery-pic" style={{backgroundImage: `url("${homepic.image_url}")`}}></div>
+                                          </div>
+                                      </div>
+                                  );
+                              }
+  
+                              if(gallery.orientation == "landscape")
+                              {
+                                  width = '1066px';
+  
+                              }
+                              else
+                              {
+                                  width = '533px';
+  
+                              }
+  
+                              return (
+                                  <div>
+                                      <div>
+                                      <div className="gallery-pic" style={{backgroundImage: `url("${homepic.image_url}")`, width: width}}></div>
+                                      </div>
+                                  </div>
+                              );
+                          }
                     })}
           </Slider>
         </div>
