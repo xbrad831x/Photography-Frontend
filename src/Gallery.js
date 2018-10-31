85adb60f13cb07bfc4c60e46acd30b10092e717d
Brandon <mercadobrandon33@yahoo.com>
2018-10-30 18:09:21 -0700

    added

--- src/Gallery.js
+++ src/Gallery.js
@@ -59,7 +59,8 @@ export default class Gallery extends Component {
                         slidesToScroll: 1,
                         autoplay: true,
                         pauseOnHover: false,
-                    }
+                    },
+                }
             ]
           }
 
